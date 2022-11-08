a94d5c4d439c7cc320404a72867370fbafd54809
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-08 16:58:34 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import img1 from '../../../images/banner/banner6.jpeg'
+import img2 from '../../../images/banner/banner5.webp'
+import img3 from '../../../images/banner/banner6.jpeg'
+import img4 from '../../../images/banner/banner5.webp'
+import './banner.css'
+import BannerItem from './BannerItem';
+
+
+const Banner = () => {
+
+    const bannerData = [
+    {
+        image: img1,
+        prev: 4,
+        id: 1,
+        next: 2
+    },
+    {
+        image: img2,
+        prev: 1,
+        id: 2,
+        next: 3
+    },
+    {
+        image: img3,
+        prev: 2,
+        id: 3,
+        next: 4
+    },
+    {
+        image: img4,
+        prev: 3,
+        id: 4,
+        next: 1
+    }
+]
+
+    return (
+        <div className="carousel w-full pt-16">
+            {
+                bannerData.map(slide => <BannerItem
+                    key={slide.id}
+                    slide={slide}>
+                </BannerItem>)
+            }
+           
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const BannerItem = ({ slide}) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative  w-full">
+            <div className='slider-img'>
+                <img src={image} alt="" className="w-full " />
+            </div>
+            <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 top-1/3">
+                <h1 className="text-5xl text-white font-semibold">Lets' Tour, Enjoy Life <br /> & Explore The World </h1>
+
+            </div>
+            <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <p className='text-white pt-20'>Let a Travel Consultant Help You Decide Where to Go. <br /> Contact Us for Information Today. <br /> A Travel Consultant Can Help You Find a Great Trip . Call Us Now.
+                </p>
+
+            </div>
+
+            <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 bottom-0 justify-end">
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
