83cfa9ec23902e7d1bd1c389ec91b795f9fbb1f3
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-09 15:25:32 +0600

    signup added

--- src/Router/Routers/Routers.js
+++ src/Router/Routers/Routers.js
@@ -4,6 +4,7 @@ import AllServices from "../../Pages/AllServiices/AllServices";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import ServiceDetails from "../../Pages/ServiceDetails/ServiceDetails";
+import Signup from "../../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
             {
                 path:'/login',
                 element:<Login></Login>
+            },
+            {
+                path:'/signup',
+                element:<Signup></Signup>
             },
             {
                 path:'/servicedetails/:id',
