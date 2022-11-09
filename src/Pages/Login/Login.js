6b425cdbe5668559e5a082dc0eaa7e0cb0cc06d5
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-09 17:35:35 +0600

    updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const Login = () => {
 
-
+    const { signIn } = useContext(AuthContext)
     const handleLogin = (event) => {
         event.preventDefault();
 
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const Signup = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+
+    const handleSignup = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+
+        createUser(email, password)
+
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
+    }
+
+
     return (
-        <div>
-            
+        <div className="hero w-full my-20 ">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+
+                    <img src="" alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <h1 className="text-5xl font-bold text-center pt-4">Sign Up!</h1>
+                    <form onSubmit={handleSignup} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn bg-orange-500" type="submit" value="Sign Up" />
+
+                        </div>
+                    </form>
+                    <p className='text-center mb-6'>Already have an account ? <Link className='font-bold text-orange-600' to='/login'>Log In</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
