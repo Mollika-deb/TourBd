2416dd52f81c4a1ec9af2f433b74b1d8c2fa6b86
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-09 18:39:13 +0600

    review updated

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const ServiceDetails = () => {
     const {title, img, description, price} = useLoaderData();
+    const {user} = useContext(AuthContext)
+
+    const handleReview = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.firstName.value ;
+        const email = user?.email || 'unregistered';
+        const message = form.message.value;
+        const phone = form.phone.value;
+    }
+
+   
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -16,7 +29,7 @@ const ServiceDetails = () => {
                     </div>
                 </div>
             </div>
-            <form >
+            <form onSubmit={handleReview}>
                 <div className="grid grid-cols-1 mt-6 lg:grid-cols-2 gap-4">
                     <input name="firstName" type="text" placeholder="First name" className="input input-bordered w-full " />
 
