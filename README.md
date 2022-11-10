d2cdcac50140da94c58ff9d987e80ce4001045b9
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-10 18:03:18 +0600

    blog added

--- README.md
+++ README.md
@@ -3,21 +3,17 @@
 ## Description
 We have been traveling around the world on a budget as much as we can. Nowadays, it is easier than ever to find cheap flights and take time off. Just get out of your comfort zone and do it!
 
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Website Overview
 
 * This is a Travale Website
 *  people can chose their best estination here.
 * people can share their opinion here.
 * There has given details about some of tourist places
-* user friendly
+* user friendly.
 
-In the project directory, you can run:
+## Website link is hare 
+(https://tourbd-2500b.web.app/) to view it in your browser.
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
 
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,10 +3,22 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1 className="text-4xl">What is the difference between SQL and NoSQL</h1>
-            <p>1. SQL pronounced as “S-Q-L” or as “See-Quel” is primarily called RDBMS or Relational Databases, whereas NoSQL is a Non-relational or Distributed <br />
+            <h1 className="text-3xl font-bold m-3 ">1. What is the difference between SQL and NoSQL</h1>
+            <p className='m-3'>1. SQL pronounced as “S-Q-L” or as “See-Quel” is primarily called RDBMS or Relational Databases, whereas NoSQL is a Non-relational or Distributed <br />
                2. Comparing SQL vs NoSQL databases, SQL databases are table-based databases, whereas NoSQL databases can be document-based, key-value pairs, and graph databases. <br />
                3. SQL databases are vertically scalable, while NoSQL databases are horizontally scalable.</p>
+            <h1 className="text-3xl font-bold m-3 ">2. What is JWT, and how does it work?</h1>
+            <p className='m-3'>JWTs or JSON Web Tokens are most commonly used to identify an authenticated user. They are issued by an authentication server and are consumed by the client-server (to secure its APIs).<br />
+            Working process of jWt token has given below: <br />
+                1. Create a JSON <br /> 2. Create a JWT signing key and decide the signing algorithm <br /> 
+                3.  Creating the “Header”. <br /> 4.  Create a signature <br /> 5. Creating the JWT <br /> 6. Verifying the JWT </p>
+            <h1 className="text-3xl font-bold m-3 ">3. What is the difference between javascript and NodeJS?</h1>
+            <p className='m-3'>
+                1. JavaScript is a proper high-level programming language used to create web scripts whereas Node.js is a run time environment built on google’s v8 engine. <br /> 2. JavaScript is executed in the browser whereas using Node.js gives us the ability to execute JavaScript outside the browser.<br /> 
+                3.  JavaScript can manipulate DOM or add HTML within whereas Node.js doesn’t have the capability to add HTML. <br /> 4.  JavaScript is mainly used to create front end web applications or develop client-side whereas Node.js is used on the back end development that is server-side development <br /> 5. JavaScript follows the standard of JavaScript when writing programs whereas Node.js is written in C++ while using the v8 engine, it runs JavaScript outside the browser. <br />  </p>
+            <h1 className="text-3xl font-bold m-3 ">4. How does NodeJS handle multiple requests at the same time?</h1>
+            <p className='m-3'>
+                NodeJS receives multiple client requests and places them into EventQueue.NodeJS is built with the concept of event-driven architecture.NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. </p>
         </div>
     );
 };
